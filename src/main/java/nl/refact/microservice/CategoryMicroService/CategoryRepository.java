57bb20901ee8a5c7ed047c535838842b101e1584
AntiEevee <eefjekarremans@hotmail.nl>
2022-01-03 13:15:46 +0100

    StartSter

---
+++ src/main/java/nl/refact/microservice/CategoryMicroService/CategoryRepository.java
@@ -0,0 +1,6 @@
+package nl.refact.microservice.CategoryMicroService;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
