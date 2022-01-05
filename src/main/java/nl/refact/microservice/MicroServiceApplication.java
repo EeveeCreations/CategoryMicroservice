3afd121458baaa92a27110856a4156de21f82c5c
AntiEevee <eefjekarremans@hotmail.nl>
2022-01-05 11:33:36 +0100

    pom toegevoegd

--- src/main/java/nl/refact/microservice/MicroServiceApplication.java
+++ src/main/java/nl/refact/microservice/MicroServiceApplication.java
@@ -2,6 +2,15 @@ package nl.refact.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class MicroServiceApplication {
@@ -10,4 +19,25 @@ public class MicroServiceApplication {
         SpringApplication.run(MicroServiceApplication.class, args);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("nl.refact.microservice.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfo(
+                "Category MicroService IKREFACT",
+                "Spring Boot REST API for IKREFACT",
+                "1.0",
+                "Terms of service",
+                new Contact("Ewout, Eefje", "https://springframework.guru/about/", "eefje@ewout.Refact"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList());
+    }
 }
